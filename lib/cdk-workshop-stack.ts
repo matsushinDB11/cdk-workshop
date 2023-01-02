f71cda50f951e02be3b00f2d908534662b1939b2
matsushinDB11 <matsushin35@gmail.com>
2023-01-03 04:32:37 +0900

    :sparkles: create lambda defines

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,7 +1,14 @@
 import {App, Stack, StackProps} from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+    /** defines as Lambda resource */
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
   }
 }
