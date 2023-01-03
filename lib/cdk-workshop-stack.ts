e6994593fbb2ff245543190bb91cc76a5387afce
matsushinDB11 <matsushin35@gmail.com>
2023-01-04 03:56:46 +0900

    :sparkles: add hitcounter

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import {HitCounter} from "./hitcounter";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
@@ -12,9 +13,13 @@ export class CdkWorkshopStack extends Stack {
       handler: 'hello.handler',
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitConter', {
+      downstream: hello
+    })
+
     /** defines an API Gateway */
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello,
+      handler: helloWithCounter.handler,
     });
   }
 }
