521430c9572aeb6afe58a048237fc71c667542d2
matsushinDB11 <matsushin35@gmail.com>
2023-01-04 02:21:33 +0900

    :art: pretteir format

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,6 +1,6 @@
-import {App, Stack, StackProps} from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
@@ -9,12 +9,12 @@ export class CdkWorkshopStack extends Stack {
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
-    })
+      handler: 'hello.handler',
+    });
 
     /** defines an API Gateway */
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
-    })
+      handler: hello,
+    });
   }
 }
